ca43bad3d096b8ad23aa0368eead1c2e8948469c
GabrielFerlima <gabriel_fernandes1995@hotmail.com>
2022-10-16 15:12:25 -0300

    Adiconando Busca na aba search de transações

--- DtMoney/src/contexts/TransactionsContext.tsx
+++ DtMoney/src/contexts/TransactionsContext.tsx
@@ -11,6 +11,7 @@ interface Transaction {
 
 interface TransactionContextType {
     transactions: Transaction[];
+    fetchTransactions: (query?: string) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -20,17 +21,27 @@ interface TransactionsProviderProps {
 export const TransactionsContext = createContext({} as TransactionContextType)
 
 export function TransactionsProvider ({ children }: TransactionsProviderProps) {
-
     const [transactions, setTransactions] = useState<Transaction[]>([])
 
-    useEffect(() => {
-        fetch('http://localhost:3333/transactions')
-            .then(response => response.json()
-                .then(data => setTransactions(data)))
+    async function fetchTransactions(query?: string) {
+        const url = new URL('http://localhost:3333/transactions')
+
+        if(query) {
+            url.searchParams.append('q', query)
+        }
+
+        const response = await fetch(url)
+        const data = await response.json()
+
+        setTransactions(data)
+    }
+
+    useEffect(()=> {
+        fetchTransactions()
     }, [])
 
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
             {children}
         </TransactionsContext.Provider>
     )
